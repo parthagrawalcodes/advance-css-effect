f4c760256307912fd7b647d3ace689535030c55d
parthagrawalcodes <parthagrawal17102004@gmail.com>
2026-01-17 15:02:03 +0530

    text filling plus border

--- script.js
+++
@@ -1,5 +0,0 @@
-addEventListener("mousemove",(e)=>{
-    console.log(e.clientX,e.clientY);
-    document.body.style.setProperty("--x",e.clientX+'px');
-    document.body.style.setProperty("--y",e.clientY+'px');
-})
